7f4c82811cbb9a11a8a2884c0eae2d65b17b6da7
nvcccu <qwer@qwer.qwer>
2015-05-03 20:43:43 +0400

    Добавил в песочницу пример запроса с подключением к базе.

--- Sandbox/Program.cs
+++ Sandbox/Program.cs
@@ -1,15 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using DAO;
+using DAO.Attributes;
+using DAO.Enums;
 
-namespace Sandbox
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
+namespace Sandbox {
+    public class DaoTest : AbstractEntity<DaoTest> {
+        public DaoTest() : base("DaoTest") {}
+
+        public enum Fields {
+            [DbType(typeof (Int64))] Id,
+            [DbType(typeof (Int32))] ParentId,
+            [DbType(typeof (String))] Text,
+            [DbType(typeof (DateTime))] DateCreated
+        }
+
+        public long Id { get; set; }
+        public long ParentId { get; set; }
+    }
+
+    internal class Program {
+        private static void Main(string[] args) {
+            Connector.ConnectionString = "Server=localhost;Port=5432;User=postgres;Password=postgres;Database=postgres;";
+
+            var q = new DaoTest()
+                .Select()
+                .Where(DaoTest.Fields.Id, PredicateCondition.Greater, 0)
+                .Where(DaoTest.Fields.ParentId, PredicateCondition.In, new List<int> {2, 18})
+                .Where(DaoTest.Fields.DateCreated, PredicateCondition.In, new List<DateTime> {new DateTime(2015, 3, 30, 21, 41, 24)})
+                .GetData()
+                .ToList();
+            var w = 0;
         }
     }
 }
